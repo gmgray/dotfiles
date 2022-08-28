46b670a87edaecdcd81e6df0b41374236daa0043
Gabriel Mil <gabriel@igm.net.pl>
2022-08-28 23:29:07 +0200

    Updated folders structure and added AVR templates

---
+++ src/avr/main.c
@@ -0,0 +1,12 @@
+/* This is a template C file for pure AVR C program. 
+ * Gabriel Mil 2022
+ */
+
+# include <avr/io.h>
+# include <util/delay.h>
+
+
+int main() {
+    _delay_ms(1000);
+    return 0;
+}
